1f753810371739c43065b43ebb1ea5adf20924ca
Maximusthr <arthurmcb321@gmail.com>
2026-03-12 21:29:05 -0300

    feat: delete skins
    Co-authored-by: Yves ribeiro de sena <Yvesena@users.noreply.github.com>

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ def menu():
         print("3. Registrar Compra")
         print("4. Cadastrar nova skin")
         print("5. Atualizar skin")
+        print("6. Deletar skin")
         print("0. Sair")
         
         opcao = input("Escolha uma opção: ")
@@ -30,7 +31,7 @@ def menu():
             id_skn = int(input("ID da Skin: "))
             valor = float(input("Valor da transação (R$): "))
             compras.registrar_compra(id_cli, id_skn, valor)
-        elif opcao == '4': # Assumindo que a opção 2 é para Inserir Skin
+        elif opcao == '4': 
             print("\n--- CADASTRAR NOVA SKIN ---")
             nome = input("Nome da Skin (Ex: AK-47 Redline): ")
             estado = input("Estado (Ex: Field-Tested): ")
@@ -39,7 +40,6 @@ def menu():
             wear = float(input("Wear Rating (Ex: 0.15): "))
            
             
-            # Chama a função que acabámos de criar
             skins.inserir_skin(nome, estado, raridade, pattern, wear)
         elif opcao == '5':
             print("\n--- ATUALIZAR SKIN ---")
@@ -50,6 +50,29 @@ def menu():
             wear = float(input("Wear Rating (Ex: 0.15): "))
 
             skins.atualizar_skin(nome, estado, raridade, pattern, wear)
+
+        elif opcao == '6':
+            print("\n" + "="*30)
+            print("   DELETAR SKIN DO STOCK")
+            print("="*30)
+            
+            print("Skins disponíveis no sistema:")
+            skins.listar_skins() 
+            
+            print("-" * 30)
+            
+            try:
+                id_remover = int(input("\nIntroduza o ID da skin que deseja apagar: "))
+                
+                confirmar = input(f"Tem a certeza que deseja remover o ID {id_remover}? (S/N): ").upper()
+                
+                if confirmar == 'S':
+                    skins.deletar_skin(id_remover)
+                else:
+                    print(" Operação cancelada pelo utilizador.")
+                    
+            except ValueError:
+                print(" Erro: Por favor, insira um número de ID válido (ex: 1, 2, 5).")    
         else:
             print("Opção inválida.")
 
--- src/skins.py
+++ src/skins.py
@@ -14,10 +14,10 @@ def inserir_skin(nome, estado, raridade, pattern, wear_rating):
             cursor.execute(sql, valores)
             conn.commit()
             
-            print(f"\n✅ Sucesso! A skin '{nome}' foi inserida no stock.")
+            print(f"\nSucesso! A skin '{nome}' foi inserida no stock.")
             
         except Exception as e:
-            print(f"\n❌ Erro ao inserir a skin: {e}")
+            print(f"\nErro ao inserir a skin: {e}")
             conn.rollback()
             
         finally:
@@ -59,6 +59,30 @@ def listar_skins():
                 print(f"ID: {skin[0]} | Nome: {skin[1]} | Estado: {skin[2]}")
         except Exception as e:
             print(f"Erro ao listar skins: {e}")
+        finally:
+            cursor.close()
+            conn.close()
+
+def deletar_skin(id_skins):
+    conn = get_conexao()
+    if conn:
+        try:
+            cursor = conn.cursor()
+            
+            sql = "DELETE FROM Skins WHERE id_skins = %s"
+            
+            cursor.execute(sql, (id_skins,))
+            
+            
+            if cursor.rowcount > 0:
+                conn.commit()
+                print(f"\n Sucesso! A skin com ID {id_skins} foi removida do sistema.")
+            else:
+                print(f"\n Aviso: Nenhuma skin encontrada com o ID {id_skins}.")
+                
+        except Exception as e:
+            print(f"\n Erro ao eliminar skin: {e}")
+            conn.rollback()
         finally:
             cursor.close()
             conn.close()
