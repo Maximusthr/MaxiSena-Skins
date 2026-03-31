02d1d82ad085a094b3bd393f62efc20fafc4c443
Maximusthr <arthurmcb321@gmail.com>
2026-03-31 19:18:08 -0300

    chore: app.py & gitignore
    Co-authored-by: Yves ribeiro de sena <Yvesena@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,7 +1,10 @@
 import os
 from flask import Flask, jsonify
+from flask import Flask, render_template
 from flask_migrate import Migrate
 from dotenv import load_dotenv
+
+from src.seed import popular_banco_se_vazio
 
 from src.database import db
 from src.models import Cliente, Vendedor, Pagamento, Skin, Compra, ItemCompra
@@ -10,6 +13,9 @@ from src.clientes import clientes_bp
 
 from src.clientes import clientes_bp
 from src.skins import skins_bp
+from src.vendedores import vendedores_bp
+from src.compras import compras_bp
+from src.pagamentos import pagamentos_bp 
 
 load_dotenv()
 
@@ -28,18 +34,24 @@ migrate = Migrate(app, db)
 # blueprint no flask
 app.register_blueprint(clientes_bp)
 app.register_blueprint(skins_bp)
+app.register_blueprint(vendedores_bp)
+app.register_blueprint(pagamentos_bp)
+app.register_blueprint(compras_bp)
 
-# --- ROTAS DE TESTE ---
 
 @app.route('/', methods=['GET'])
-def index():
-    return jsonify({"mensagem": "API do Mercado CS2 rodando com sucesso!"})
+def pagina_inicial():
+    # O Flask vai procurar automaticamente dentro da pasta /templates
+    return render_template('index.html')
 
-# --- INICIALIZAÇÃO DO BANCO ---
-# Este bloco cria as tabelas automaticamente assim que o app rodar pela primeira vez
+#@app.route('/', methods=['GET'])
+#def index():
+    #return jsonify({"mensagem": "API do Mercado CS2 rodando com sucesso!"})
+
+# Cria as tabelas automaticamente assim que o app rodar pela primeira vez
 with app.app_context():
-    db.create_all()
-    print("Tabelas sincronizadas com sucesso no banco de dados!")
+    db.create_all()               # Cria as tabelas se não existirem
+    popular_banco_se_vazio(db)    # Injeta os dados se estiver vazio
 
 if __name__ == '__main__':
     app.run(debug=True)
